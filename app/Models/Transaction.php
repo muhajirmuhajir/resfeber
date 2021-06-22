11e5e4237f708150e22b47ac2c943ecc49e3e37b
muhajirmuhajir <hajir.nf@gmail.com>
2021-06-22 14:57:11 +0700

    create Transaction Relations

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function paket()
+    {
+        return $this->belongsTo(Paket::class, 'paket_id');
+    }
+
+    public function tempatWisata()
+    {
+        return $this->belongsTo(TempatWisata::class, 'tempat_wisata_id');
+    }
 }
