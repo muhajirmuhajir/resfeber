e0a2b42101ecb8d5efbcf78fb93ea7de160552bc
muhajirmuhajir <hajir.nf@gmail.com>
2021-06-22 15:32:26 +0700

    create Ulasan Relations

--- app/Models/Ulasan.php
+++ app/Models/Ulasan.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Ulasan extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tempatWisata()
+    {
+        return $this->belongsTo(TempatWisata::class, 'tempat_wisata_id');
+    }
+
 }
