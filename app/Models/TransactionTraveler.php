da8db04dfdb481daf89fa9da75987dd1dd368e47
Qonita Nur Farhana <qonitafarhana@student.ub.ac.id>
2021-06-22 15:01:50 +0700

    add transaction function

--- app/Models/TransactionTraveler.php
+++ app/Models/TransactionTraveler.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionTraveler extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function transaction()
+    {
+        return $this->belongsTo(TransactionDB::class, 'transaction_id');
+    }
 }
