e5ed80c00a38ecf318a3b59474d625a89802ad29
muhajirmuhajir <hajir.nf@gmail.com>
2021-06-22 15:54:44 +0700

    create TempatWisata Relations

--- app/Models/TempatWisata.php
+++ app/Models/TempatWisata.php
@@ -8,4 +8,47 @@ use Illuminate\Database\Eloquent\Model;
 class TempatWisata extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function facilities()
+    {
+        return $this->hasMany(Facility::class);
+    }
+
+    public function produks()
+    {
+        return $this->hasMany(Produk::class);
+    }
+
+    public function locations()
+    {
+        return $this->hasMany(Location::class);
+    }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function media()
+    {
+        return $this->hasMany(MediaTempatWisata::class);
+    }
+
+    public function ulasan()
+    {
+        return $this->hasMany(Ulasan::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function pakets()
+    {
+        return $this->hasMany(Paket::class);
+    }
+
 }
