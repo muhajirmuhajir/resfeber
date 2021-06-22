e9e3b905a6dac747f9c035765507df11dc402f78
Qonita Nur Farhana <qonitafarhana@student.ub.ac.id>
2021-06-22 14:39:02 +0700

    create model and migration

--- database/migrations/2021_06_22_072540_create_transaction_travelers_table.php
+++ database/migrations/2021_06_22_072540_create_transaction_travelers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTransactionTravelersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transaction_travelers', function (Blueprint $table) {
             $table->id();
+            $table->integer('transaction_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->longText('address');
             $table->string('phone_number');
-            $table->integer('role');
-            $table->rememberToken();
+            $table->integer('age');
             $table->timestamps();
         });
     }
@@ -34,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transaction_travelers');
     }
 }
--- database/migrations/2021_06_22_073700_create_additional_needs_table.php
+++ database/migrations/2021_06_22_073700_create_additional_needs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAdditionalNeedsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('additional_needs', function (Blueprint $table) {
             $table->id();
+            $table->integer('tempat_wisata_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->longText('address');
-            $table->string('phone_number');
-            $table->integer('role');
-            $table->rememberToken();
+            $table->longText('description');
             $table->timestamps();
         });
     }
@@ -34,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('additional_needs');
     }
 }
