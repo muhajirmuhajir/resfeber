823207788d522b259a6caba3a8d667a350096f31
Qonita Nur Farhana <qonitafarhana@student.ub.ac.id>
2021-06-23 09:31:56 +0700

    add footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -830,56 +830,7 @@
     </div>
     <!-- == main content ends == -->
 
-    <!-- footer section starts
-================================================== -->
-    <footer id="dtr-footer">
-
-        <!--== footer main starts ==-->
-        <div class="dtr-footer-main dtr-box-layout">
-
-            <!--== row starts ==-->
-            <div class="row">
-
-                <!-- column 1 starts -->
-                <div class="col-12 col-md-6 footer-col-1"> <img src="assets/images/logo-dark.png" alt="logo">
-                    <p class="dtr-mt-20">&copy; Copyright 2021. Made by <a href="https://themeforest.net/user/tansh">Tansh</a></p>
-                </div>
-                <!-- column 1 ends -->
-
-                <!-- column 2 starts -->
-                <div class="col-12 col-md-6 footer-col-2">
-
-                    <!-- list starts -->
-                    <ul class="dtr-list-inline">
-                        <li><a href="#home" class="dtr-scroll-link">Home</a></li>
-                        <li><a href="#about" class="dtr-scroll-link">About</a></li>
-                        <li><a href="#properties" class="dtr-scroll-link">Properties</a></li>
-                        <li><a href="#services" class="dtr-scroll-link">Services</a></li>
-                        <li><a href="#contact" class="dtr-scroll-link">Contact</a></li>
-                    </ul>
-                    <!-- list ends -->
-
-                    <!-- social starts -->
-                    <ul class="dtr-social dtr-social-list dtr-mt-20">
-                        <li><a href="#" class="dtr-instagram" target="_blank" title="instagram"></a></li>
-                        <li><a href="#" class="dtr-skype" target="_blank" title="skype"></a></li>
-                        <li><a href="#" class="dtr-google" target="_blank" title="google"></a></li>
-                        <li><a href="#" class="dtr-twitter" target="_blank" title="twitter"></a></li>
-                    </ul>
-                    <!-- social ends -->
-
-                </div>
-                <!-- column 2 ends -->
-
-            </div>
-            <!--== row ends ==-->
-
-        </div>
-        <!--== footer main ends ==-->
-
-    </footer>
-    <!-- footer section ends
-================================================== -->
+    @include('includes.footer')
 
 </div>
 <!-- #dtr-wrapper ends -->
