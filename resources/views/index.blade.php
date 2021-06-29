45a87cec81d3e267ac65fd149d9e9415d70e6d3d
muhajirmuhajir <hajir.nf@gmail.com>
2021-06-29 14:38:58 +0700

    change service icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -219,7 +219,8 @@
 
             <!-- column 2 starts -->
             <div class="col-12 col-md-3 dtr-feature dtr-feature-top dtr-feature-square">
-                <div class="dtr-feature-img bg-medium-blue color-dark-blue"><i class="icon-city"></i></div>
+                <div class="dtr-feature-img icon-center" style="background-color: #EFDBFE;"><img
+                        src="{{url('frontpage/assets/images/ic-terupdate.png')}}" alt="" srcset=""></div>
                 <h5>Terupdate</h5>
                 <p>Seluruh informasi pada resfeber diperbarui dalam jangka waktu tertentu sehingga informasi yang
                     disajikan up to date.</p>
@@ -228,7 +229,8 @@
 
             <!-- column 3 starts -->
             <div class="col-12 col-md-3 dtr-feature dtr-feature-top dtr-feature-square">
-                <div class="dtr-feature-img bg-medium-red color-dark-brown"><i class="icon-user-tag"></i></div>
+                <div class="dtr-feature-img icon-center" style="background-color: #FFDBDB;"><img
+                        src="{{url('frontpage/assets/images/ic-mitra.png')}}" alt="" srcset=""></div>
                 <h5>Mitra</h5>
                 <p>Resfeber bekerjasama dengan lebih dari 100 mitra agen tour travel yang menyediakan paket wisata.</p>
             </div>
@@ -242,8 +244,8 @@
 
             <!-- column 1 starts -->
             <div class="col-12 col-md-3 dtr-feature dtr-feature-top dtr-feature-square">
-                <div class="dtr-feature-img bg-medium-green color-dark-green"><i class="icon-door-open"></i>
-                </div>
+                <div class="dtr-feature-img icon-center" style="background-color: #DBE7FE;"><img
+                        src="{{url('frontpage/assets/images/ic-kredibel.png')}}" alt="" srcset=""></div>
                 <h5>Kredibel</h5>
                 <p>Seluruh informasi pada resfeber valid dan dapat dipercaya.</p>
             </div>
@@ -251,8 +253,8 @@
 
             <!-- column 2 starts -->
             <div class="col-12 col-md-3 dtr-feature dtr-feature-top dtr-feature-square">
-                <div class="dtr-feature-img bg-medium-purple color-dark-purple"><i class="icon-walking"></i>
-                </div>
+                <div class="dtr-feature-img icon-center" style="background-color: #FEE8DB"><img
+                        src="{{url('frontpage/assets/images/ic-akurat.png')}}" alt="" srcset=""></div>
                 <h5>Akurat</h5>
                 <p>Seluruh informasi pada resfeber sudah dilakukan verifikasi dan validasi langsung ke lapangan.</p>
             </div>
@@ -260,7 +262,8 @@
 
             <!-- column 3 starts -->
             <div class="col-12 col-md-3 dtr-feature dtr-feature-top dtr-feature-square">
-                <div class="dtr-feature-img bg-light-red color-dark-brown"><i class="icon-wind"></i></div>
+                <div class="dtr-feature-img icon-center" style="background-color: #DBFEE7;"><img
+                        src="{{url('frontpage/assets/images/ic-lengkap.png')}}" alt="" srcset=""></div>
                 <h5>Lengkap</h5>
                 <p>Seluruh informasi pada resfeber mencakup banyak aspek secara detail, diataranya sejarah, jam
                     operasional, fasilitas, biaya tiket dan lainnya.</p>
