bbeb42ca009a3ab9bb226d745b9518030028c0dd
Qonita Nur Farhana <qonitafarhana@student.ub.ac.id>
2021-06-29 09:39:55 +0700

    carddddddddd

--- resources/views/pages/wisata.blade.php
+++ resources/views/pages/wisata.blade.php
@@ -38,44 +38,63 @@
                 </div>
             </form>
         </div>
+
+
         <div class="konten-box">
             <h1>Jawa Timur</h1>
             <p>Daftar destinasi wisata sekitar Jawa Timur.</p>
-            <div class="row">
-                <div class="col-sm">
-                    <div class="card" style="width: 18rem;">
-                        <img src="assets/images/ranu.png" class="card-img-top">
-                        <div class="card-body">
-
-                            <h5 class="card-title">Ranu Kumbolo</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                      </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+              <div class="card h-100">
+                <img src="assets/images/ranu.png" class="card-img-top">
+                <div class="card-body">
+                  <span class="dtr-blog-cat">Jawa Timur</span>
+                  <h5 class="card-title">Ranu Kumbolo</h5>
+                  <p class="card-text">Probolinggo</p>
                 </div>
-                <div class="col-sm">
-                    <div class="card" style="width: 18rem;">
-                        <img src="assets/images/ranu.png" class="card-img-top ">
-                        <div class="card-body">
-
-                            <h5 class="card-title">Ranu Kumbolo</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                      </div>
+                <div class="card-footer">
+                    <span class="badge badge-secondary">kamar mandi</span>
+                    <span class="badge badge-secondary">kamar mandi</span>
+                    <span class="badge badge-secondary">kamar mandi</span>
+                    <span class="badge badge-secondary">kamar mandi</span>
                 </div>
-                <div class="col-sm">
-                    <div class="card" style="width: 18rem;">
-                        <img src="assets/images/ranu.png" class="card-img-top">
-                        <div class="card-body">
-
-                            <h5 class="card-title">Ranu Kumbolo</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                      </div>
-                </div>
+              </div>
             </div>
-
-
+            <div class="col">
+              <div class="card h-100">
+                <img src="assets/images/ranu.png" class="card-img-top">
+                <div class="card-body">
+                  <span class="dtr-blog-cat">Jawa Timur</span>
+                  <h5 class="card-title">Ranu Kumbolo</h5>
+                  <p class="card-text">Probolinggo</p>
+                </div>
+                <div class="card-footer">
+                    <span class="badge badge-secondary">kamar mandi</span>
+                    <span class="badge badge-secondary">kamar mandi</span>
+                    <span class="badge badge-secondary">kamar mandi</span>
+                    <span class="badge badge-secondary">kamar mandi</span>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="assets/images/ranu.png" class="card-img-top">
+                <div class="card-body">
+                  <span class="dtr-blog-cat">Jawa Timur</span>
+                  <h5 class="card-title">Ranu Kumbolo</h5>
+                  <p class="card-text">Probolinggo</p>
+                </div>
+                <div class="card-footer">
+                    <span class="badge badge-secondary">kamar mandi</span>
+                    <span class="badge badge-secondary">kamar mandi</span>
+                    <span class="badge badge-secondary">kamar mandi</span>
+                    <span class="badge badge-secondary">kamar mandi</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+
     </section>
 </div>
 @endsection
