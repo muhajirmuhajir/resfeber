fb2e5b294e1fbaf026c720a2cde5a1189134942e
muhajirmuhajir <hajir.nf@gmail.com>
2021-07-06 10:53:09 +0700

    fix route active link

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -7,28 +7,26 @@
                     <span class="nav-text">Dashboard</span>
                 </a>
             </li>
-            <li class="{{Route::currentRouteName() == 'admin.tempat-wisata.index' ? 'mm-active' : ''}}"><a
-                    href="{{route('admin.tempat-wisata.index')}}"
-                    class="ai-icon {{Route::currentRouteName() == 'admin.tempat-wisata.index' ? 'mm-active' : ''}}"
-                    aria-expanded="false">
+            <li class="{{Str::startsWith(Route::currentRouteName(),'admin.tempat-wisata') ? 'mm-active' : ''}}"><a
+                    href="{{route('admin.tempat-wisata.index')}}" class="ai-icon" aria-expanded="false">
                     <i class="flaticon-381-layer-1"></i>
                     <span class="nav-text">Tempat Wisata</span>
                 </a>
             </li>
-            <li class="{{Route::currentRouteName() == 'admin.paket-wisata.index' ? 'mm-active' : ''}}"><a
-                    href="{{route('admin.paket-wisata.index')}}"
-                    class="ai-icon {{Route::currentRouteName() == 'admin.paket-wisata.index' ? 'mm-active' : ''}}"
-                    aria-expanded="false">
+            <li class="{{Str::startsWith(Route::currentRouteName(),'admin.paket-wisata') ? 'mm-active' : ''}}"><a
+                    href="{{route('admin.paket-wisata.index')}}" class="ai-icon" aria-expanded="false">
                     <i class="flaticon-381-layer-1"></i>
                     <span class="nav-text">Paket Wisata</span>
                 </a>
             </li>
-            <li><a href="travel.html" class="ai-icon" aria-expanded="false">
+            <li class="{{Str::startsWith(Route::currentRouteName(),'admin.travel') ? 'mm-active' : ''}}"><a
+                    href="travel.html" class="ai-icon" aria-expanded="false">
                     <i class="flaticon-381-layer-1"></i>
                     <span class="nav-text">Travel</span>
                 </a>
             </li>
-            <li><a href="transaksi.html" class="ai-icon" aria-expanded="false">
+            <li class="{{Str::startsWith(Route::currentRouteName(),'admin.transaksi') ? 'mm-active' : ''}}"><a
+                    href="transaksi.html" class="ai-icon" aria-expanded="false">
                     <i class="flaticon-381-layer-1"></i>
                     <span class="nav-text">Transaksi</span>
                 </a>
