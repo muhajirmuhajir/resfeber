3ffbb8a04a47898f253d4bbf5a0115d003640a5c
Qonita Nur Farhana <qonitafarhana@student.ub.ac.id>
2021-07-21 12:06:47 +0700

    edit travel

--- resources/views/pages/travel.blade.php
+++ resources/views/pages/travel.blade.php
@@ -9,7 +9,7 @@
 
     <!-- hero section starts -->
     <section id="home" class="dtr-section dtr-box-layout dtr-hero-section-top-padding">
-        <h1 class="h1">Paket Wisata</h1>
+        <h1 class="h1">Paket Travel</h1>
         @forelse ($travels as $travel)
         <div class="paket-container mb-4"
             style="background-image: url('{{url('frontpage/assets/images/paket1.png')}}');">
@@ -23,7 +23,7 @@
                             <p class="text-justify text-dark">{{$paket->description}}</p>
                             @endforeach
                             <p class="text-dark">Harga mulai</p>
-                            <h3 class="h3 text-danger">{{$paket->price}}</h3>
+                            <h3 class="h3 text-danger">Rp. {{$paket->price}}</h3>
                             <a href="{{route('detailTravel',['id'=> $travel->id])}}"
                                 class="dtr-btn btn-dark-blue d-block mt-4">Lihat
                                 Paket</a>
